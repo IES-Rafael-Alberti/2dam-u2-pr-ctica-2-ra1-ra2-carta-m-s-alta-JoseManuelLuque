ee4d1cba9e7468e8a9ee77e3543317bfb960bc07
José Manuel Luque <jluqgon214@g.educaand.es>
2023-11-13 19:18:02 +0100

    Test

--- app/src/main/java/com/jluqgon214/cartamasalta/Carta.kt
+++ app/src/main/java/com/jluqgon214/cartamasalta/Carta.kt
@@ -1,4 +1,5 @@
 package com.jluqgon214.cartamasalta
 
-class Carta(nombre: Naipes, palos: Palos, puntosMin: Int, puntosMax: Int, idDrawable: String) {
+class Carta(nombre: Naipes, palos: Palos, puntosMin: Int, puntosMax: Int, idDrawable: Int) {
+
 }
